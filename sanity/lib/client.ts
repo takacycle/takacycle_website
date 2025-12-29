e7a2f6f37f0e6188636e1dff2ae781580518f7ef
nanayeb34 <nanayeb34@gmail.com>
2025-12-29 08:25:16 +0000

    Fix Sanity client type import

--- sanity/lib/client.ts
+++ sanity/lib/client.ts
@@ -1,6 +1,5 @@
 import { createClient } from 'next-sanity';
 import imageUrlBuilder from '@sanity/image-url';
-import { SanityImageSource } from '@sanity/image-url/lib/types/types';
 
 export const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!;
 export const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET!;
@@ -15,6 +14,7 @@ export const client = createClient({
 
 const builder = imageUrlBuilder(client);
 
-export function urlFor(source: SanityImageSource) {
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export function urlFor(source: any) {
   return builder.image(source);
 }
